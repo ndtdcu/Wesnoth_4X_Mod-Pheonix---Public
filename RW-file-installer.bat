e5e91a1e1e626f6b9360a714ee47dd13328d5193
ndtdcu <ndtdcug@gmail.com>
2026-07-16 23:36:24 -0500

    new bat

--- RW-file-installer.bat
+++ RW-file-installer.bat
@@ -8,6 +8,7 @@ echo.
 
 set "RW_PATH=C:\Program Files (x86)\Steam\steamapps\common\Rusted Warfare"
 set "MODS_PATH=%RW_PATH%\mods\units"
+set "MOD_OUTPUT_NAME=Wesnoth_4X_Mod-Pheonix1"
 set "ZIP_NAME=Wesnoth_4X_Mod-Pheonix.zip"
 set "TEMP_DIR=temp_extract"
 
@@ -33,11 +34,33 @@ if exist "%TEMP_DIR%" rmdir /S /Q "%TEMP_DIR%"
 echo Extracting ZIP...
 powershell -command "Expand-Archive -Path '%ZIP_NAME%' -DestinationPath '%TEMP_DIR%' -Force"
 
-echo Removing old mod folder...
-if exist "%MODS_PATH%\Wesnoth_4X_Mod-Pheonix" rmdir /S /Q "%MODS_PATH%\Wesnoth_4X_Mod-Pheonix"
+echo Detecting extracted mod folder...
 
-echo Copying new mod folder...
-xcopy "%TEMP_DIR%\Wesnoth_4X_Mod-Pheonix" "%MODS_PATH%\Wesnoth_4X_Mod-Pheonix" /E /I /Y
+set "EXTRACTED_FOLDER="
+
+rem --- Try exact folder names ---
+for /d %%F in ("%TEMP_DIR%\*") do (
+    if /i "%%~nxF"=="Wesnoth_4X_Mod-Pheonix" set "EXTRACTED_FOLDER=%TEMP_DIR%\%%~nxF"
+    if /i "%%~nxF"=="Wesnoth_4X_Mod-Pheonix-main" set "EXTRACTED_FOLDER=%TEMP_DIR%\%%~nxF"
+)
+
+rem --- Fallback: find folder containing mod-info.txt ---
+if not defined EXTRACTED_FOLDER (
+    for /r "%TEMP_DIR%" %%F in (mod-info.txt) do (
+        set "EXTRACTED_FOLDER=%%~dpF"
+        goto found_mod_folder
+    )
+)
+
+:found_mod_folder
+
+echo Using extracted folder: %EXTRACTED_FOLDER%
+
+echo Removing old TEST mod folder...
+if exist "%MODS_PATH%\%MOD_OUTPUT_NAME%" rmdir /S /Q "%MODS_PATH%\%MOD_OUTPUT_NAME%"
+
+echo Copying new TEST mod folder...
+xcopy "%EXTRACTED_FOLDER%" "%MODS_PATH%\%MOD_OUTPUT_NAME%" /E /I /Y
 
 echo Cleaning temp folder...
 rmdir /S /Q "%TEMP_DIR%"
@@ -45,6 +68,8 @@ rmdir /S /Q "%TEMP_DIR%"
 echo.
 echo ============================================
 echo        Installation Complete!
+echo Installed to TEST folder:
+echo "%MODS_PATH%\%MOD_OUTPUT_NAME%"
 echo ============================================
 echo.
 
